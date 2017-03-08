b5664aa5ce08da55cbb5bd594278d2de0fbce75a
as27 <andreas@apxp.info>
2017-03-08 17:51:45 +0100

    observer test works

--- observer.go
+++ observer.go
@@ -1,19 +1,61 @@
 package govuegui
 
+import (
+	"time"
+)
+
+// ObserverRefreshTime defines how often the routinge of the
+// observer checks for changes.
+var ObserverRefreshTime = 100 * time.Millisecond
+
 type Observer struct {
+	stop        chan int
+	strings     map[*string]string
+	subscribers []EventFunc
 }
 
+type EventFunc func(event, key, oldval string)
+
 func NewObserver() *Observer {
-	return &Observer{}
+	return &Observer{
+		stop:    make(chan int),
+		strings: make(map[*string]string),
+	}
 }
 
 func (o *Observer) Start() {
+	go func() {
+		for {
+			select {
+			case <-time.Tick(time.Millisecond * 100):
+				for p, val := range o.strings {
+					if *p != val {
+						o.Emmit("Value Changed", *p, val)
+						o.strings[p] = *p
+					}
+				}
+			case <-o.stop:
+				return
+			}
+		}
+	}()
+}
 
+func (o *Observer) Emmit(event, key, oldval string) {
+	for _, f := range o.subscribers {
+		f(event, key, oldval)
+	}
 }
 
 func (o *Observer) Stop() {
-
+	o.stop <- 0
 }
+
+func (o *Observer) Subscribe(ef EventFunc) {
+	o.subscribers = append(o.subscribers, ef)
+}
+
 func (o *Observer) AddString(s *string) {
+	o.strings[s] = *s
 
 }
--- observer_test.go
+++ observer_test.go
@@ -1,12 +1,27 @@
 package govuegui
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAddString(t *testing.T) {
+
+}
 
 func TestObserver(t *testing.T) {
 	myString := ""
+	outerString := "Outer"
 	o := NewObserver()
 	o.AddString(&myString)
 	o.Start()
+	o.Subscribe(func(event, key, oldval string) {
+		fmt.Println(outerString)
+		fmt.Println(event, key, oldval)
+	})
 	myString = "Me"
+	//time.Sleep(time.Second)
+	myString = "You"
+	//time.Sleep(time.Second)
 	o.Stop()
 }
